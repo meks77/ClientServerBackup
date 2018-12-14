59f5b692ffc72bc9d8e36f9d4d5619beb946c2f8
Markus Hager <meks77@gmail.com>
2018-12-14 06:57:21 +0100

    ClientServerBackup-8 As Admin I'd like to see the connection stats in the client list
    Default constructor wos gone after the use of the Builder

--- backend/src/main/java/at/meks/backupclientserver/backend/domain/Client.java
+++ backend/src/main/java/at/meks/backupclientserver/backend/domain/Client.java
@@ -2,15 +2,17 @@ package at.meks.backupclientserver.backend.domain;
 
 import io.jsondb.annotation.Document;
 import io.jsondb.annotation.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Collection;
 import java.util.Date;
 
 @Document(collection = "clients", schemaVersion = "1.0")
-@Getter @Setter @Builder
+@Getter @Setter @Builder @NoArgsConstructor @AllArgsConstructor
 public class Client {
 
     @Id
